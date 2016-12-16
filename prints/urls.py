49795b32ccd55c3696a7bda073af448abdb67dd5
Piotr Wójcik <piotr_to@op.pl>
2016-12-16 19:40:18 +0100

    collection of prints

--- prints/urls.py
+++ prints/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.search, name='search'),
-    url(r'^all/$', views.search, name='all'),
+    url(r'^collection/$', views.collection, name='collection'),
     url(r'^by_name/$', views.search, name='by_name'),
     url(r'^add/$', views.search, name='add'),
 
--- prints/views.py
+++ prints/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def search(request):
     return render(request, "index.html")
 
+
+def collection(request):
+    paths = ["/img/data/Acta et literae_{}.jpg".format(i) for i in range(7)]
+    return render(request, "collection.html", {'paths': paths})
